a408454b023f96aacb8d5778e6f666cc6b9c7dc8
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-06-12 20:34:41 +0300

    Добавил аннотацию

--- src/test/java/ReqresTests.java
+++ src/test/java/ReqresTests.java
@@ -1,16 +1,25 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
 public class ReqresTests {
+
+    @BeforeAll
+    public static void setUp() {
+        RestAssured.baseURI = "https://reqres.in";
+        RestAssured.basePath = "/api";
+    }
 
     @Test
     void getSingleUserTest() {
         given()
                 .log().all()
-                .get("https://reqres.in/api/users/2")
+                .get("/users/2")
                 .then()
                 .log().all()
                 .statusCode(200)
@@ -21,7 +30,7 @@ public class ReqresTests {
     void getSingleUserNotFoundTest() {
         given()
                 .log().all()
-                .get("https://reqres.in/api/users/23")
+                .get("/users/23")
                 .then()
                 .log().all()
                 .statusCode(404);
@@ -32,7 +41,7 @@ public class ReqresTests {
     void deleteSingleUserTest() {
         given()
                 .log().all()
-                .delete("https://reqres.in/api/users/2")
+                .delete("/users/2")
                 .then()
                 .log().all()
                 .statusCode(204);
@@ -46,12 +55,12 @@ public class ReqresTests {
                 .contentType(JSON)
                 .log().all()
                 .when()
-                .post("https://reqres.in/api/register")
+                .post("/register")
                 .then()
                 .log().all()
                 .statusCode(200)
                 .body("id",is(4))
-                .body("token", is("QpwL5tke4Pnpja7X4"));
+                .body("token", notNullValue());
     }
 
     @Test
@@ -62,7 +71,7 @@ public class ReqresTests {
                 .contentType(JSON)
                 .log().all()
                 .when()
-                .put("https://reqres.in/api/users/2")
+                .put("/users/2")
                 .then()
                 .log().all()
                 .statusCode(200)
